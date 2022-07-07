386f441b82327e0b55caf62bbb2d026f581c5099
fenas <fenas@hurreytech.com>
2022-07-07 15:29:42 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      blackish: "#484847",
+      offWhite: "#f8f3eb",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
